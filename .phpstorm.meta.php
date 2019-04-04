30f4a8acb0c3db077ce28f0efb580a49e3bbbbc6
aleksandr <asidorov@wakeapp.ru>
2019-04-04 16:10:28 +0300

    HUB-179

---
+++ .phpstorm.meta.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace PHPSTORM_META;
+
+override(\Wakeapp\Bundle\DbalBundle\Manager\DbalManager::fetchDto(0), type(0));
--- Factory/DtoResolverFactory.php
+++ Factory/DtoResolverFactory.php
@@ -4,19 +4,31 @@ declare(strict_types=1);
 
 namespace Wakeapp\Component\DtoResolver\Factory;
 
-use Wakeapp\Component\DtoResolver\Dto\CollectionDtoResolverInterface;
 use Wakeapp\Component\DtoResolver\Dto\DtoResolverInterface;
 use Wakeapp\Component\DtoResolver\Exception\InvalidDtoClassException;
 
 class DtoResolverFactory
 {
     /**
+     * @deprecated use DtoResolverFactory::create() instead
+     *
      * @param string $className
      * @param array $data
      *
      * @return DtoResolverInterface
      */
     public function createDto(string $className, array $data): DtoResolverInterface
+    {
+        return self::create($className, $data);
+    }
+
+    /**
+     * @param string $className
+     * @param array $data
+     *
+     * @return DtoResolverInterface
+     */
+    public static function create(string $className, array $data): DtoResolverInterface
     {
         if (!is_subclass_of($className, DtoResolverInterface::class)) {
             throw new InvalidDtoClassException($className, DtoResolverInterface::class);
